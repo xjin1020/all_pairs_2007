fe3c2d02257f1d18d3fe3b1059f2d1c5c2b1a8c2
xjin1020 <xin_jin@wuhan.(none)>
2016-04-14 16:13:55 -0700

    r

--- run.sh
+++ run.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-echo "1k vectors"
-java -jar all_pairs_2007.jar data/1000 1000 0.8
-java -jar all_pairs_2007.jar data/1000 1000 0.9
-java -jar all_pairs_2007.jar data/1000 1000 0.95
 echo "10k vectors"
-java -jar all_pairs_2007.jar data/10000 10000 0.8
-java -jar all_pairs_2007.jar data/10000 10000 0.9
-java -jar all_pairs_2007.jar data/10000 10000 0.95
-echo "100k vectors"
-java -jar all_pairs_2007.jar data/100000 100000 0.8
-java -jar all_pairs_2007.jar data/100000 100000 0.9
-java -jar all_pairs_2007.jar data/100000 100000 0.95
+echo "2_2:"
+java -Xmx13000m -jar all_pairs_2007_2_2.jar data/10000 10000 0.5
+java -Xmx13000m -jar all_pairs_2007_2_2.jar data/10000 10000 0.7
+java -Xmx13000m -jar all_pairs_2007_2_2.jar data/10000 10000 0.8
+java -Xmx13000m -jar all_pairs_2007_2_2.jar data/10000 10000 0.9
+java -Xmx13000m -jar all_pairs_2007_2_2.jar data/10000 10000 0.95
+echo "2_3:"
+java -Xmx13000m -jar all_pairs_2007_2_3.jar data/10000 10000 0.5
+java -Xmx13000m -jar all_pairs_2007_2_3.jar data/10000 10000 0.7
+java -Xmx13000m -jar all_pairs_2007_2_3.jar data/10000 10000 0.8
+java -Xmx13000m -jar all_pairs_2007_2_3.jar data/10000 10000 0.9
+java -Xmx13000m -jar all_pairs_2007_2_3.jar data/10000 10000 0.95
+#echo "100k vectors"
+#java -Xmx13000m -jar all_pairs_2007_2_2.jar data/100000 100000 0.95
+#java -Xmx13000m -jar all_pairs_2007_2_2.jar data/100000 100000 0.9
+#java -Xmx13000m -jar all_pairs_2007_2_2.jar data/100000 100000 0.8
+#java -Xmx13000m -jar all_pairs_2007_2_2.jar data/100000 100000 0.7
+#java -Xmx13000m -jar all_pairs_2007_2_2.jar data/100000 100000 0.5
